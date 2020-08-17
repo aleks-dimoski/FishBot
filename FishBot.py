4fbb86a66bbfc605c04cd4a6eeba55468def90b5
aleks-dimoski <aleksdimoski890@gmail.com>
2020-08-16 21:54:48 -0400

    Model accuracy improved; needs more data

--- FishBot.py
+++ FishBot.py
@@ -23,37 +23,6 @@ batch_size = 16
 learning_rate = 5e-4
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-
-def plot_image_prediction(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-
-  plt.imshow(np.squeeze(img), cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
-                                100*np.max(predictions_array),
-                                true_label),
-                                color=color)
-def plot_value_prediction(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
-
 def img_mod(img):
     # Image modification specifications #
     wd = 360
@@ -73,9 +42,9 @@ def load_data():
     direc_neg = '/negative/'
 
     train_pos = [(direc_pos+x) for x in os.listdir(direc+direc_pos) if x.endswith('.jpg')]
-    pos_labels = [[0] for x in train_pos]
+    pos_labels = [[1] for x in train_pos]
     train_neg = [(direc_neg+x) for x in os.listdir(direc+direc_neg) if x.endswith('.jpg')]
-    neg_labels = [[1] for x in train_neg]
+    neg_labels = [[0] for x in train_neg]
 
     imgs = train_pos + train_neg  # [img_mod(Image.open(direc + x)) for x in traimage resize tensorflowin_pos or train_neg]
 
@@ -95,7 +64,6 @@ def load_data():
     labels = (labels).astype(np.float32)
 
     print("\nData Loading Complete")
-    print(labels)
 
     return (imgs, labels)
 
@@ -107,13 +75,11 @@ def cnn_model():
     pool = tf.keras.layers.MaxPool2D
 
     model = tf.keras.Sequential([
-        conv2D(filters=num_filters, kernel_size=(4, 4), strides=2),
+        conv2D(filters=num_filters, kernel_size=(3, 3), strides=2),
         tf.keras.layers.BatchNormalization(),
         pool((2, 2)),
         conv2D(filters=num_filters * 2, kernel_size=(3, 3), strides=2),
-        pool((2, 2)),
         conv2D(filters=num_filters * 4, kernel_size=(3, 3), strides=2),
-        pool((2, 2)),
         flatten(),
         dense(512),
         dense(1, activation=None)
@@ -157,16 +123,16 @@ def train_model(fname, optimizer=tf.keras.optimizers.Adamax(5e-3)):
     model.save(fname)
 
 
-train_model('fish_finder', optimizer=optimizer)
+#train_model('fish_finder', optimizer=optimizer)
 
 model = tf.keras.models.load_model('fish_finder')
 
 test_images = os.getcwd() + '/_LABELED-FISHES-IN-THE-WILD/fish/'
 test_images2 = os.getcwd() + '/_LABELED-FISHES-IN-THE-WILD/negative/'
 test1 = [(test_images+x) for x in os.listdir(test_images) if x.endswith('.jpg')]
-pos_labels = [0 for x in test1]
+pos_labels = [1 for x in test1]
 test2 = [(test_images2+x) for x in os.listdir(test_images2) if x.endswith('.jpg')]
-neg_labels = [1 for x in test2]
+neg_labels = [0 for x in test2]
 test = test1 + test2
 labels = pos_labels + neg_labels
 indices = [x for x in range(len(labels))]
@@ -179,8 +145,6 @@ labels = np.array([labels[x] for x in indices])
 test = tf.convert_to_tensor((test/255.).astype(np.float32), dtype=tf.float32)
 labels = (labels).astype(np.float32)
 
-test_results = model(test)
-
 # Plots the first X test images, their predicted label, and the true label
 # Color correct predictions in blue, incorrect predictions in red
 num_rows = 5
@@ -188,9 +152,9 @@ num_cols = 4
 num_images = num_rows*num_cols
 plt.figure(figsize=(4*num_cols, 2*num_rows))
 for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image_prediction(i, test_results, labels, test)
-  #plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  #plot_value_prediction(i, test_results, labels)
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plt.imshow(test[i])
+    plt.xlabel("{:0.02f} ({})".format(np.squeeze(model(test)[i]), labels[i]), color=('red' if np.squeeze(model(test)[i]) >= 0 else 'blue'))
+    plt.grid(False)
 
 plt.show()
